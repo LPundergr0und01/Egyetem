2765a15f0123ef75b31c68dcc0b54945db78b2d4
LPundergr0und01 <LPundergr0und01@users.noreply.github.com>
2018-11-26 18:41:29 +0200

    AB L4 DONE

---
+++ III. Semester/Adatbazis/L4/srim1761_lab4_02_II.sql
@@ -0,0 +1,146 @@
+﻿/*
+--1. Adjuk meg a legrégebben gyártott `Volkswagen`
+	 márkájú autó(k) típusát! (Tipusok.TipusNev)
+	 */
+SELECT Tipusok.TipusNev, Autok.GyartasiEv into #tmp1
+FROM Tipusok
+JOIN Autok
+ON Tipusok.TipusID = Autok.TipusID
+JOIN Markak
+ON Markak.MarkaID = Tipusok.MarkaID
+WHERE Markak.MarkaNev LIKE '%Volkswagen%' 
+
+declare @min INT = (select min (#tmp1.GyartasiEv) from #tmp1)
+
+select #tmp1.TipusNev
+from #tmp1
+where #tmp1.GyartasiEv = @min
+
+drop table #tmp1
+
+/*2. Adjuk meg autótípusokon belül az 
+	autók számát! (Tipusok.TipusNev, AutokSzama)*/
+
+select t.TipusNev, count(a.AutoKod)
+from Tipusok as t
+join Autok as a on a.TipusID = t.TipusID
+group by t.TipusNev
+
+
+/*3. Adjuk meg autómárkákon belül típusok szerint az 
+	autók átlagos napidíját! (Markak.MarkaNev, Tipusok.TipusNev, AtlagNapidij)*/
+	 
+
+select Markak.MarkaNev, Tipusok.TipusNev, avg(Autok.NapiDij) 
+from Markak
+join Tipusok
+on Tipusok.MarkaID = Markak.MarkaID
+join Autok
+on Autok.TipusID  = Tipusok.TipusID
+group by Markak.MarkaNev, Tipusok.TipusNev
+
+
+/*
+4. Adjuk meg azon autómárkákat, melyek esetén az autók 
+	értékelésének (Csillag) minimuma NEM kisebb, mint 2! (Markak.MarkaNev)
+	*/
+
+select Markak.MarkaNev, min(Autok.Csillag) as m into #tmp2
+from Markak
+join Tipusok
+on Tipusok.MarkaID = Markak.MarkaID
+join Autok
+on Autok.TipusID = Tipusok.TipusID
+group by Markak.MarkaNev
+
+select #tmp2.MarkaNev 
+from #tmp2
+where #tmp2.m >2
+
+drop table #tmp2
+----------------------------------------------------------------------------
+select Markak.MarkaNev--, min(Autok.Csillag) as m 
+from Markak
+join Tipusok
+on Tipusok.MarkaID = Markak.MarkaID
+join Autok
+on Autok.TipusID = Tipusok.TipusID
+group by Markak.MarkaNev
+having min(autok.Csillag) > 2
+/*
+5. Adjuk meg azon személyeket, akik az elmúlt egy évben	
+	(aktuális naphoz viszonyítva!) min 2x bérelték ki a legdrágább 
+	autót (NapiDij szerint)! (Berlok.SzemSzam, Berlok.Nev)
+	*/
+
+
+
+--SELECT Berlok.BerloID , Berlok.Nev 
+SELECT Berlok.BerloID , Berlok.Nev , Autok.NapiDij INTO #TMP3
+FROM Berlok
+JOIN Berel
+ON Berel.BerloID = Berlok.BerloID
+JOIN Autok
+ON Autok.AutoKod = Berel.AutoKod
+GROUP BY Berlok.BerloID , Berlok.Nev, Autok.NapiDij
+HAVING Autok.NapiDij = MAX(Autok.NapiDij)
+
+/*
+
+6. Adjuk meg azon bérlőket, akik egyetlen autót sem 
+	béreltek ki! (Berlok.Nev)
+
+*/
+
+
+SELECT Berlok.Nev
+FROM Berlok
+LEFT JOIN Berel
+ON Berlok.BerloID = Berel.BerloID
+WHERE Berel.BerloID IS NULL
+
+/*
+7. Adjuk meg azon autótípus(oka)t, amely(ek) esetén 
+	minden színből válogathatunk! (Tipusok.TipusNev)
+*/
+
+DECLARE @COLORNUM int
+
+SELECT @COLORNUM = COUNT(Szinek.SzinKod)
+FROM Szinek
+
+SELECT Tipusok.TipusNev
+FROM Tipusok, Autok
+WHERE Tipusok.TipusID=Autok.TipusID
+GROUP BY TipusNev
+HAVING COUNT(DISTINCT SzinKod)=@COLORNUM
+
+/*
+8. Rendezzük növekvő sorrendbe az egyes autótípusokat aszerint,
+	 hogy összességében milyen árban bérelték ki őket 
+	(Berel tábla Ar mezőjét figyelembe véve)! (Tipus.Nev, OsszBerlesAr)
+*/
+SELECT TipusID, sum(Ar) AS OsszBerlesAr INTO #TAB1
+FROM Berel, Autok
+WHERE Berel.AutoKod=Autok.AutoKod
+GROUP BY TipusID
+	
+SELECT TipusNev, OsszBerlesAr
+FROM Tipusok,#TAB1
+WHERE Tipusok.TipusID=#TAB1.TipusID
+ORDER BY OsszBerlesAr
+
+DROP TABLE #TAB1
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ III. Semester/Adatbazis/L4/srim1761_lab4_02_III.sql
@@ -0,0 +1,107 @@
+﻿--9
+/*
+Módosítsuk azon diák(ok) telefonszámát ’000000000’-ra, aki(k) a 'Kreiger, Thiel and Schmeler' bentlakásban lakik/laknak!
+*/
+
+DECLARE @NUM VARCHAR(12) = '000000000'
+
+UPDATE Diakok
+SET Telefonszam = @NUM
+FROM Diakok
+JOIN Lakik
+ON Lakik.DiakID = Diakok.DiakID
+JOIN Bentlakasok
+ON Bentlakasok.BentlakasID = Lakik.BentlakasID
+WHERE Bentlakasok.Nev LIKE '%Kreiger, Thiel and Schmeler%'
+
+--10
+/*
+Töröljük a 'Layla Latimer' nevű diákot!
+*/
+
+DELETE FROM Lakik
+WHERE DiakID=(
+	SELECT DiakID
+	FROM Diakok
+	WHERE Nev LIKE '%Layla Latimer%'
+)
+DELETE FROM Diakok
+WHERE Nev LIKE '%Layla Latimer%'
+
+
+DECLARE @DID INT;
+
+SELECT @DID=DiakID
+FROM Diakok
+WHERE Nev like 'Layla Latimer'
+
+DELETE FROM Lakik
+WHERE DiakID=@DID
+
+DELETE FROM Diakok
+WHERE DiakID=@DID
+
+
+--11
+/*
+A 100-nál nagyobb szobaszámmal rendelkező bentlakás(ok)hoz szúrjunk be 5 darab 1 ágyas szobát!
+*/
+
+INSERT INTO SzobaTipusok VALUES( (SELECT MAX(SzobaTipusID)+1 FROM SzobaTipusok),'1 agyas', 1, 300)
+
+DECLARE @SZTID INT;
+
+SELECT @SZTID=SzobaTipusID 
+FROM SzobaTipusok
+WHERE AgyakSzama=1
+
+INSERT INTO Szobak(BentlakasID,SzobaTipusID,SzobakSzama)
+SELECT BentlakasID, @SZTID,5
+FROM Bentlakasok
+WHERE SzobaSzam>100
+
+
+--12
+/*
+A biológia szakos diákokat költöztessük a ‘Maggio’ bentlakásba, és a történelem szakosokat a ‘Kshlerin and Sons’ bentlakásba!
+*/
+
+DECLARE @BID1 INT;
+DECLARE @BID2 INT;
+DECLARE @SZID1 INT;
+DECLARE @SZID2 INT;
+
+SELECT @BID1=BentlakasID
+FROM Bentlakasok
+WHERE Nev like 'Maggio'
+
+SELECT @SZID1=SzakID
+FROM Szakok
+WHERE SzakNev like 'biologia'
+
+update Lakik
+SET BentlakasID =@BID1
+WHERE DiakID in
+(
+SELECT DiakID
+FROM Diakok
+WHERE SzakID =@SZID1
+)
+
+
+SELECT @BID2=BentlakasID
+FROM Bentlakasok
+WHERE Nev like 'Kshlerin and Sons'
+
+SELECT @SZID2=SzakID
+FROM Szakok
+WHERE SzakNev like 'tortenelem'
+
+update Lakik
+SET BentlakasID =@BID2
+WHERE DiakID in
+(
+SELECT DiakID
+FROM Diakok
+WHERE SzakID =@SZID2
+)
